64de1e0c9199f04d82d447eb43cdd0f194352971
Keltaisten <keltaisten@gmail.com>
2021-10-09 21:49:42 +0200

    Addition

---
+++ src/main/java/classstructureio/Calculator.java
@@ -0,0 +1,14 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Please give me two numbers:");
+        Scanner scann = new Scanner(System.in);
+        int firstNumb = scann.nextInt();
+        int secondNumb = scann.nextInt();
+        System.out.println(firstNumb + " + " + secondNumb);
+        System.out.println(firstNumb + secondNumb);
+    }
+}
