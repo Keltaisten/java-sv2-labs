ab1bd76c8d892b3de81d5faf14d3e5563eb15e06
Keltaisten <keltaisten@gmail.com>
2021-10-14 00:00:37 +0200

    packages

---
+++ src/main/java/packages/lastname/Lastname.java
@@ -0,0 +1,13 @@
+package packages.lastname;
+
+public class Lastname {
+    private String last;
+
+    public Lastname(String last) {
+        this.last = last;
+    }
+
+    public String getLast() {
+        return last;
+    }
+}
