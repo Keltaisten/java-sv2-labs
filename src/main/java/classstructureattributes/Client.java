e31f5f352847c0ddebce280af4e87256bb079795
Keltaisten <keltaisten@gmail.com>
2021-10-15 22:27:42 +0200

    Regroup

--- src/main/java/classstructureattributes/Client.java
+++
@@ -1,7 +0,0 @@
-package classstructureattributes;
-
-public class Client {
-    String name;
-    int yearOfBirth;
-    String address;
-}
--- src/main/java/javaapi/Upper.java
+++
@@ -1,8 +0,0 @@
-package javaapi;
-
-public class Upper {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!".toUpperCase());
-    }
-}
