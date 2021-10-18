49907859414158ef87f057566f64928dabc13449
Keltaisten <keltaisten@gmail.com>
2021-10-18 20:28:16 +0200

    introControl-Boot-90%

--- training-solutions/src/main/java/introcontrol/BootRental.java
+++ training-solutions/src/main/java/introcontrol/BootRental.java
@@ -9,112 +9,66 @@ public class BootRental {
         Scanner scann = new Scanner(System.in);
 
         System.out.println("Kérem adja meg hányan vennék igénybe a kölcsönzést");
-        //number = scann.nextInt();
-        number = 5;
-        if (number <= 2) {
-            System.out.println(number + " vitték el.");
-            System.out.println(10 - number + " fő mehet még utánuk.");
-        } else {
-            if (number == 3) {
-                System.out.println(number + " vitték el.");
-                System.out.println(10- number + " fő mehet még utánuk.");
-            } else {
-                if (number <= 5) {
-                    System.out.println(number + " vitték el.");
-                    System.out.println(10 - number +" fő mehet még utánuk.");
-                } else {
-                    if (number <= 7) {
-                        System.out.println(number + " vitték el.");
-                        System.out.println(10 - number + " fő mehet még utánuk.");
-                    } else {
-                        if (number <= 8) {
-                            System.out.println(number + " vitték el.");
-                            System.out.println(10 - number +" fő mehet még utánuk.");
-                        }else {
-                            if (number <= 10) {
-                                System.out.println(number + " vitték el.");
-                                System.out.println(10 - number +" fő mehet még utánuk.");
-                            }
-                        }
-                    }
+        number = scann.nextInt();
 
-                }
-            }
-        }
-
-
-
-
-        numbers(1);
-        numbers(2);
-        numbers(3);
-        numbers(4);
-        numbers(5);
-        numbers(6);
-        numbers(7);
-        numbers(8);
-        numbers(9);
-        numbers(10);
-
-    }
-
-
-    public void soutLines(int number){
-        System.out.println(number + " vitték el.");
-        System.out.println(10 - number + " fő mehet még utánuk.");
+        numbers(number);
     }
 
     public static void numbers(int numb){
-        boolean boat5 = false;
-        boolean boat3 = false;
-        boolean boat2 = false;
+        String taken = "Az elvitt csónak/csónakok: ";
         int remain = 10;
-        int originalNumber = numb;
+        String place;
+
+        if(numb>10){
+            place = "és nincs több szabad hely. Maradtak még a parton.";
+        }
+        else{
+            place = "és nincs több szabad hely.";
+        }
+
 
         if(numb>5){
             numb -= 5;
             remain -=5;
-            boat5 = true;
+            taken = taken + "5-ös, ";
             if(numb/3 > 0){
                 numb -= 3;
                 remain -=3;
-                boat3= true;
+                taken = taken + "3-as, ";
             }
 
             if(numb > 0){
                 numb -= 2;
                 remain -= 2;
-                boat2 = true;
+                taken = taken + "2-es, ";
             }
         }
         else{
             if(numb/4 > 0){
                 numb -= 5;
                 remain -=5;
-                boat5 = true;
+                taken = taken + "5-ös, ";
             }
 
             if(numb/3 > 0){
                 numb -= 3;
                 remain -=3;
-                boat3= true;
+                taken = taken + "3-as, ";
             }
 
             if(numb > 0){
                 numb -= 2;
                 remain -= 2;
-                boat2 = true;
+                taken = taken + "2-es, ";
             }
         }
 
+        if(remain == 0){
+            System.out.println(taken + place);
+        }
+        else{
+            System.out.println(taken + "és " + remain + " fő mehet még utánuk.");
+        }
 
-        System.out.println("Csapat létszáma: " + originalNumber + ", 5 személyes : " + boat5 +
-                ", 3 személyes :" + boat3 + ", 2 személyes: " + boat2
-                + ", Férőhely maradt: " + remain);
     }
-
-    public static void numbers2(int numb) {
-
-    }
-
 }
