b7b71485187f1077e516d28f424f39aacf664dfe
Keltaisten <keltaisten@gmail.com>
2021-10-22 01:47:57 +0200

    freezer

---
+++ training-solutions/src/main/java/arraylist/Freezer.java
@@ -0,0 +1,26 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Freezer {
+    public static void main(String[] args) {
+        List<String> freezerList = new ArrayList<>();
+
+        freezerList.add("Rakott karfiol");
+        freezerList.add("Bolognai");
+        freezerList.add("Húsleves");
+        freezerList.add("Pörkölt");
+        freezerList.add("Túrógombóc");
+
+        System.out.println(freezerList.toString());
+        System.out.println(freezerList.size());
+
+        freezerList.remove("Bolognai");
+        freezerList.remove("Pörkölt");
+        
+        System.out.println(freezerList.toString());
+        System.out.println(freezerList.size());
+    }
+}
