b546a5e6e0272bcc84f6c8a28d48099854dcb4f5
Keltaisten <keltaisten@gmail.com>
2021-10-23 20:25:17 +0200

    winners

---
+++ training-solutions/src/main/java/math/random/RandomDraw.java
@@ -0,0 +1,20 @@
+package math.random;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class RandomDraw {
+    public static void main(String[] args) {
+        List<String> members = new ArrayList<>(Arrays.asList("Pisti", "Olga", "Sándor", "Gabi", "Bandi",
+                "Vali", "Holli", "Fiona", "Leona", "Imola"));
+
+        Random rand = new Random();
+        int random1= rand.nextInt(1,5);
+        int random2= rand.nextInt(6,10);
+
+        System.out.println("Az első nyertesünk: " + members.get(random1));
+        System.out.println("A második nyertes: " + members.get(random2));
+    }
+}
