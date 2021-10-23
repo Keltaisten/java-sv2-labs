e53186a5e943a060d2b765b57e2331651453c904
Keltaisten <keltaisten@gmail.com>
2021-10-23 13:29:04 +0200

    Week

--- training-solutions/src/main/java/finalmodifier/Week.java
+++ training-solutions/src/main/java/finalmodifier/Week.java
@@ -11,12 +11,13 @@ public class Week {
             //Ezt final static nélkül sem lehetne növelni csökkenteni. Módosítani lehet.
         public final static List<String> DAYS_OF_WEEK = Arrays.asList("Hetfő", "Kedd", "Szerda",
         "Csütörtök", "Péntek", "Szombat", "Vasárnap");
+
     public static void main(String[] args) {
         //Tömbbő készített listán nem lehet az elemek számát növelni csökkenteni. Módosítani lehet. Mindet lehet.
 //        List<String> DAYS_OF_WEEK = Arrays.asList("Hetfő", "Kedd", "Szerda",
 //        "Csütörtök", "Péntek", "Szombat", "Vasárnap");
         DAYS_OF_WEEK.set(1,"Szerda");
-        DAYS_OF_WEEK.add("Hello there");
+//        DAYS_OF_WEEK.add("Hello there");
         System.out.println(DAYS_OF_WEEK.get(1));
         System.out.println(DAYS_OF_WEEK);
     }
