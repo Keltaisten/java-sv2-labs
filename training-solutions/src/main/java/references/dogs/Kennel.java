b247f2e9aad7892e8e4e48d9e4073842a1f15a8f
Keltaisten <keltaisten@gmail.com>
2021-10-26 23:18:45 +0200

    Dog

---
+++ training-solutions/src/main/java/references/dogs/Kennel.java
@@ -0,0 +1,14 @@
+package references.dogs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kennel {
+    public static void main(String[] args) {
+//        List<Dog> dogs = new ArrayList<>();
+//        Dog dog1 = new Dog("Név", 3,"szin");
+//        dogs.add(dog1);
+
+
+    }
+}
