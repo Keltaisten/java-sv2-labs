c3905e42b1d8a4ebcd9b8b7f51528836c061f5a5
Keltaisten <keltaisten@gmail.com>
2021-10-27 17:37:36 +0200

    Capsules 70%

---
+++ training-solutions/src/main/java/compositionlist/Capsules.java
@@ -0,0 +1,37 @@
+package compositionlist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Capsules {
+    private List<String> cupHolder = new ArrayList<>();
+
+    public void addLast(String cup){
+        cupHolder.add(cup);
+    }
+
+    public void addFirst(String cup){
+        cupHolder.add(0,cup);
+    }
+
+    public void removeFirst(){
+        cupHolder.remove(0);
+    }
+
+    public void removeLast(){
+        cupHolder.remove(cupHolder.get(cupHolder.size()-1));
+    }
+
+    public List<String> getColors(){
+        return cupHolder;
+    }
+
+    public static void main(String[] args) {
+        Capsules capsules = new Capsules();
+        capsules.addLast("ot");
+        capsules.addFirst("negy");
+        capsules.addFirst("ketto");
+        capsules.addLast("harom");
+    }
+}
