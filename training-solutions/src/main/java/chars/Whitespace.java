f3e2ec8868344a3d94ddc400853687adb87754ac
Keltaisten <keltaisten@gmail.com>
2021-10-28 23:06:03 +0200

    White_solved_finally

--- training-solutions/src/main/java/chars/Whitespace.java
+++ training-solutions/src/main/java/chars/Whitespace.java
@@ -1,43 +1,15 @@
 package chars;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Whitespace {
-    public List<String> makeWhitespaceToStar(String text){
-        List<String> wooords = new ArrayList<>();
-        List<String> wooords2 = new ArrayList<>();
-//        for(char i: text.toCharArray()){
-//            if(i == ' '){
-//                wooords.add("*");
-//            }else{
-//                wooords.add(text.charAt(i));
-//            }
-//        }
-
-//        for(int i = 0; i < text.length();i++){
-//            if(text.toCharArray()[i] == ' '){
-//                text.toCharArray()[i] = '*';
-////                text[i] = '*';
-//            }
-////            if(text.indexOf(' ') == ' '){
-////                i = ' ';
-////            }
-//        }
-
-        for(int i = 0; i < text.length();i++){
-            if(text.toCharArray()[i] == ' '){
-                wooords2.add("*");
-//                text[i] = '*';
-            }else{
-                wooords2.add(String.valueOf(text.toCharArray()[i]));
+    public String makeWhitespaceToStar(String text){
+        char[] textArray = text.toCharArray();
+        for(int i = 0;i< textArray.length;i++){
+            if(textArray[i] == ' '){
+                textArray[i] = '*';
             }
-//            if(text.indexOf(' ') == ' '){
-//                i = ' ';
-//            }
         }
-        
-        return wooords2;
+        String newText = new String(textArray);
+        return newText;
     }
 
     public static void main(String[] args) {
