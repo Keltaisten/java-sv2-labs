d24bc815ec3321901f3bbec232aac4c896c60d15
Keltaisten <keltaisten@gmail.com>
2021-10-30 01:21:55 +0200

    Division update2

--- training-solutions/src/main/java/mathproblems/Division.java
+++ training-solutions/src/main/java/mathproblems/Division.java
@@ -1,17 +1,26 @@
 package mathproblems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Division {
     public void getDivisors(int number){
+        List<String> divirors = new ArrayList<>();
         int numberCopy = number;
-        int steps = 0;
+        int steps = 1;
+        divirors.add(0,(""));
+//        divirors.add(1,("" + number + " osztója: " + number));
         for(int i = 1; i<= numberCopy/i;i++){
             if(number%i == 0){
-                System.out.println(number + " osztója: " + i);
+                divirors.add((steps-1),("" + number + " osztója: " + i));
+//                System.out.println(number + " osztója: " + i);
                 if(numberCopy/i != i)
-                System.out.println(number + " osztója: " + (numberCopy/i));
+                    divirors.add(steps,("" + number + " osztója: " + numberCopy/i));
+//                System.out.println(number + " osztója: " + (numberCopy/i));
                 steps++;
             }
         }
+        System.out.println(divirors);
 //        System.out.println(number + " osztója: " + number);
         System.out.println(steps);
     }
