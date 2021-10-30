ceea1207fa6a7426a6e7bdfe306be7904100e646
Keltaisten <keltaisten@gmail.com>
2021-10-30 12:14:00 +0200

    Percent

--- training-solutions/src/main/java/numbers/Percent.java
+++ training-solutions/src/main/java/numbers/Percent.java
@@ -1,12 +1,40 @@
 package numbers;
 
+import java.util.Scanner;
+
 public class Percent {
-    public double getValue(int number, int percentOfNumber){
-        return (double)number*percentOfNumber/100;
+    public double getValue(int number, int percent){
+        return (double)number*percent/100;
+    }
+
+    public double getBase(int percentOfNumber, int percent){
+        return (double)percentOfNumber/percent*100;
+    }
+
+    public double getPercent(int number, int percentOfNumber){
+        return (double)percentOfNumber/number*100;
     }
 
     public static void main(String[] args) {
-        Percent percent = new Percent();
-        System.out.println(percent.getValue(200,60));
+        int number;
+        int percent;
+        int percentOfNumber;
+        Percent per = new Percent();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kérem adjon meg egy számot:");
+        number = scanner.nextInt();
+        System.out.println("Kérem adjon meg mégegy számot:");
+        percent = scanner.nextInt();
+        System.out.println("Az első számnak a második szám szerinti százaléka: " + per.getValue(number,percent));
+        System.out.println("Kérem adjon meg egy számot:");
+        percentOfNumber = scanner.nextInt();
+        System.out.println("Kérem adjon meg mégegy számot:");
+        percent = scanner.nextInt();
+        System.out.println("Az első szám minek a második szám szerinti százaléka: " + per.getBase(percentOfNumber,percent));
+        System.out.println("Kérem adjon meg egy számot:");
+        percentOfNumber = scanner.nextInt();
+        System.out.println("Kérem adjon meg mégegy számot:");
+        percent = scanner.nextInt();
+        System.out.println(per.getPercent(200,60));
     }
 }
