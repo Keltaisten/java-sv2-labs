6fe0865ecc817e652e6420bc050d8deb3e011ca2
Keltaisten <keltaisten@gmail.com>
2021-10-30 11:09:05 +0200

    Division getRightNumbers added

--- training-solutions/src/main/java/mathproblems/Division.java
+++ training-solutions/src/main/java/mathproblems/Division.java
@@ -8,12 +8,13 @@ public class Division {
         List<String> divirors = new ArrayList<>();
         int steps = 1;
         divirors.add(0,(""));
-        for(int i = 1; i<= number/i;i++){
+        for(int i = 1; i <= number/i; i++){
             if(number%i == 0){
                 divirors.add((steps-1),("" + number + " osztója: " + i));
-                if(number/i != i)
-                    divirors.add(steps,("" + number + " osztója: " + number/i));
-                steps++;
+                if(number/i != i){
+                    divirors.add(steps, ("" + number + " osztója: " + number / i));
+                    steps++;
+                }
             }
         }
         for(String elements:divirors){
@@ -24,5 +25,15 @@ public class Division {
     public static void main(String[] args) {
         Division division = new Division();
         division.getDivisors(2310);
+        int[] array = {1, 5, 6, 7, 8, 4};
+        division.getRightNumbers(array);
+    }
+
+    public void getRightNumbers(int[] numbers){
+        for(int i = 0; i < numbers.length; i++){
+            if(i != 0 && numbers[i]%i == 0){
+                System.out.print(i + " ");
+            }
+        }
     }
 }
