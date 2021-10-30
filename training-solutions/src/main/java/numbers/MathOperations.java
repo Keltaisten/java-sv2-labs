3282220f9edb6dbeadbbb575ddda132eb6f1eb1c
Keltaisten <keltaisten@gmail.com>
2021-10-30 12:29:21 +0200

    MathOperations

--- training-solutions/src/main/java/numbers/MathOperations.java
+++ training-solutions/src/main/java/numbers/MathOperations.java
@@ -1,5 +1,20 @@
 package numbers;
 
-public class MathOperations {
+import java.util.Scanner;
 
+public class MathOperations {
+    public static void main(String[] args) {
+        double result;
+        double resultFromUser;
+        Scanner scanner = new Scanner(System.in);
+        result = ((1+2)*3.0-2)/7;
+        System.out.println("Kérem adja meg a számítás eredményét");
+        System.out.println("((1+2)*3.0-2)/7");
+        resultFromUser = scanner.nextDouble();
+        if(Math.abs(result-resultFromUser)<0.0001){
+            System.out.println("A válasz helyes!");
+        }else{
+            System.out.println("A válasz helytelen.");
+        }
+    }
 }
