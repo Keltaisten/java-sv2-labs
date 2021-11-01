618c3fcbd9e384ff6130cee22c2eec896fe91787
Keltaisten <keltaisten@gmail.com>
2021-11-01 22:30:16 +0100

    Scanner

--- training-solutions/src/main/java/stringscanner/StringScanner.java
+++ training-solutions/src/main/java/stringscanner/StringScanner.java
@@ -8,15 +8,22 @@ import java.util.Scanner;
 public class StringScanner {
 
     public static void main(String[] args) {
-        Scanner scann = new Scanner(System.in);
+        Scanner scannerSystem = new Scanner(System.in);
         System.out.println("Kérem adjon meg egy összetett mondatot");
-        String str = scann.nextLine();
-        Scanner scann2 = new Scanner(str);
-        List<String> list = new ArrayList<>();
-        while (scann2.hasNext()){
-            list.add(scann2.next());
+        String strSystem = scannerSystem.nextLine();
+        Scanner scannerStr = new Scanner(strSystem);
+        while (scannerStr.hasNext()){
+            System.out.println(scannerStr.next());
         }
-        System.out.println(list);
+
+        System.out.println("Kérem adjon meg mégegy összetett mondatot");
+        String strSystem2 = scannerSystem.nextLine();
+
+        Scanner scannerStr2 = new Scanner(strSystem2).useDelimiter(", ");
+
+        while(scannerStr2.hasNext()){
+            System.out.println(scannerStr2.next());
+        }
     }
 
 
