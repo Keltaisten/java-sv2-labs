a810c0563bd8fbc0c52a32aa1399cfcaca6a8f54
Keltaisten <keltaisten@gmail.com>
2021-11-01 22:31:39 +0100

    Scanner finished

--- training-solutions/src/main/java/stringscanner/StringScanner.java
+++ training-solutions/src/main/java/stringscanner/StringScanner.java
@@ -1,17 +1,17 @@
 package stringscanner;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class StringScanner {
 
     public static void main(String[] args) {
         Scanner scannerSystem = new Scanner(System.in);
+
         System.out.println("Kérem adjon meg egy összetett mondatot");
         String strSystem = scannerSystem.nextLine();
+
         Scanner scannerStr = new Scanner(strSystem);
+
         while (scannerStr.hasNext()){
             System.out.println(scannerStr.next());
         }
@@ -25,44 +25,4 @@ public class StringScanner {
             System.out.println(scannerStr2.next());
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    public void testScanner(){
-//
-//    Scanner s = new Scanner(System.in);
-//
-//    System.out.println("Kérlek, adj meg egy öszetett mondatot!");
-//    s.nextLine();
-//    List<String> words = new ArrayList<>();
-//    int i = 0;
-//    while (s.hasNext() && i<20){
-//        words.add(s.next());
-//        i++;
-//    }
-////        System.out.println(Arrays.toString(words));
-//    }
-
-//    public static void main(String[] args) {
-//        StringScanner stringScanner = new StringScanner();
-//        System.out.println(stringScanner.testScanner());
-////    for(int j =0; s.hasNext() && j<20;j++){
-////        words.add(s.next());
-////    }
-////    System.out.println();
-//    }
 }
