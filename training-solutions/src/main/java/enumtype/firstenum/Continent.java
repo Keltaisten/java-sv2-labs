34d393c8e0f7a654cab366b85ea941bc682a9905
Keltaisten <keltaisten@gmail.com>
2021-11-01 15:16:47 +0100

    Continent fix

--- training-solutions/src/main/java/enumtype/firstenum/Continent.java
+++ training-solutions/src/main/java/enumtype/firstenum/Continent.java
@@ -2,15 +2,4 @@ package enumtype.firstenum;
 
 public enum Continent {
     EURÓPA, ÉSZAKAMERIKA, DÉLAMERIKA, AFRIKA, AUSZTRÁLIA, ANTARKTIKA, ÁZSIA;
-
-//    private final String value;
-//    Continent(String value) {
-//        this.value = value;
-//    }
-//
-//    public String getValue(){
-//        return value;
-//    }
-
-
 }
