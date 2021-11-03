e8df5510600080951cf6b8efc7021888f69ea1ab
Keltaisten <keltaisten@gmail.com>
2021-11-04 00:20:40 +0100

    Dragon

--- training-solutions/src/test/java/junit5assert/DragonTest.java
+++ training-solutions/src/test/java/junit5assert/DragonTest.java
@@ -2,8 +2,7 @@ package junit5assert;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DragonTest {
 
@@ -17,5 +16,39 @@ public class DragonTest {
     @Test
     void testNumberOfHeads(){
         assertEquals(new Dragon("Baron", 5,3432).getNumberOfHeads(),5);
+
+        assertTrue(new Dragon("Baron", 5,3432).getNumberOfHeads() == 5);
+
+        assertFalse(new Dragon("Baron", 5,3432).getNumberOfHeads() == 4);
+    }
+
+    @Test
+    void testHeight(){
+        assertEquals(new Dragon("Baron", 5,3432.5).getHeight(),3432.5014, 0.005);
+    }
+
+    @Test
+    void testNull(){
+        Dragon dragon1 = new Dragon("Baron", 5,3432.5);
+        Dragon dragon2 = new Dragon(null, 5,3432.5);
+
+        assertNull(dragon2.getName());
+        assertNotNull(dragon1.getName());
+    }
+
+    @Test
+    void testSameObjects(){
+        Dragon dragon1 = new Dragon("Baron", 5,3432.5);
+        Dragon dragon2 = dragon1;
+
+        assertSame(dragon1,dragon2);
+    }
+
+    @Test
+    void testNotSameObjects(){
+        Dragon dragon1 = new Dragon("Baron", 5,3432.5);
+        Dragon dragon2 = new Dragon("Baron", 5,3432.5);
+
+        assertNotSame(dragon1,dragon2);
     }
 }
