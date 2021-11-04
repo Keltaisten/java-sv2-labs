fb635b9b262e09f4c4eea1d909c1e2a2c7a9c8b2
Keltaisten <keltaisten@gmail.com>
2021-11-04 22:53:30 +0100

    Division

---
+++ training-solutions/src/main/java/controliteration/division/Division.java
@@ -0,0 +1,34 @@
+package controliteration.division;
+
+import java.util.Scanner;
+
+public class Division {
+    public StringBuilder divideNumbers(int numbMaximum, int numberFromUser){
+        StringBuilder stringNumbers = new StringBuilder();
+        int numberOfLoops = 1;
+        int numberDividers = numberFromUser;
+        boolean bool = true;
+        while(numberDividers<numbMaximum){
+            if(bool){
+                bool = false;
+            }else {
+                stringNumbers.append(", ");
+            }
+            stringNumbers.append(numberDividers);
+            numberOfLoops++;
+            numberDividers = numberFromUser * numberOfLoops;
+        }
+
+        return stringNumbers;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Division division = new Division();
+        System.out.println("Adjon meg egy pozitív egész számot! A program ennél kisebb számokat fog kiíírni.");
+        int numbMaximum = scanner.nextInt();
+        System.out.println("Hánnyal osztható számokat szeretne kapni?");
+        int numbDivider = scanner.nextInt();
+        System.out.println(division.divideNumbers(numbMaximum, numbDivider));
+    }
+}
