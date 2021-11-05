81a837c775c7b2418ac0ba02f233fcacf1b0179e
Keltaisten <keltaisten@gmail.com>
2021-11-06 00:23:48 +0100

    eXit

--- training-solutions/src/main/java/controliteration/exit/Exit.java
+++ training-solutions/src/main/java/controliteration/exit/Exit.java
@@ -1,5 +1,13 @@
 package controliteration.exit;
 
-public class Exit {
+import java.util.Scanner;
 
+public class Exit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        do{
+            System.out.println("Az ügyfélszolgálat menüje jelenleg nem elérhető. Kilépés: x billentyű megnyomásával");
+//            String s = scanner.nextLine();
+        }while("x".equals(scanner.nextLine().toLowerCase()));
+    }
 }
