6eb9bbbf09e7e93f96828832650c3cea210f7896
Keltaisten <keltaisten@gmail.com>
2021-11-06 17:43:39 +0100

    Forbidden Sevens finished

--- training-solutions/src/main/java/controladvanced/numbers/ForbiddenSevens.java
+++ training-solutions/src/main/java/controladvanced/numbers/ForbiddenSevens.java
@@ -6,32 +6,16 @@ public class ForbiddenSevens {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Kérem adjon meg egy pozitív egész számot");
-        Integer number = scanner.nextInt();
+        int number = scanner.nextInt();
 
-        for(Integer i = number; i < 200;i++){
-            Integer data = Integer.parseInt(String.valueOf(i.toString().toCharArray()[i.toString().toCharArray().length-1])) ;
-//            boolean bool = data == number;
-            if(i %number == 0){
+        for(Integer i = number; i >= number;i++){
+            if(i %7 == 0){
                 System.out.println("x");
-            }else if(i %number != 0 && data == number){
-//            }else if(i %7 != 0 && i.toString().toCharArray()[i.toString().toCharArray().length-1] == 7){
+                continue;
+            }else if(Integer.toString(i).contains("7")){
                 break;
-            }else{
-            System.out.println(i);
             }
-//            System.out.println("some: " + i.toString().toCharArray()[i.toString().toCharArray().length-1]);
-//            System.out.println("some: " + data);
-//            System.out.println(bool);
+            System.out.println(i);
         }
-
-//        while(number %7 != 0 && number.toString().toCharArray()[number.toString().toCharArray().length-1] == 7){
-//            if(number %7 == 0){
-//
-//            }
-//
-//        }
-//        while(number %7 == 0 && Character.toChars(number)[0] == 0);
-
-
     }
 }
