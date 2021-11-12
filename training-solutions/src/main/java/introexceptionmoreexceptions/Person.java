760ebe446ac2294eda21403824aa9bcc78dbf5d7
Keltaisten <keltaisten@gmail.com>
2021-11-12 23:31:34 +0100

    Person

---
+++ training-solutions/src/main/java/introexceptionmoreexceptions/Person.java
@@ -0,0 +1,34 @@
+package introexceptionmoreexceptions;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person(null, 32);
+        System.out.println(person.toString());
+        System.out.println(person.getName());
+        System.out.println(person.getName().toUpperCase());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
