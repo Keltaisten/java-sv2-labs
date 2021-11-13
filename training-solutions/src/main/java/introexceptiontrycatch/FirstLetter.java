b68d08f595aa4b7949ab9d045ada64911bd5e726
Keltaisten <keltaisten@gmail.com>
2021-11-13 22:55:03 +0100

    FirstLetter

---
+++ training-solutions/src/main/java/introexceptiontrycatch/FirstLetter.java
@@ -0,0 +1,27 @@
+package introexceptiontrycatch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FirstLetter {
+    ArrayList<String> words = new ArrayList<>(Arrays.asList("alma", "kifli", "szekrény", "monitor"));
+
+    public void writeAllTheWords(){
+        for(int i = 0; i <= words.size();i++){
+            try {
+            System.out.println(words.get(i));
+            }
+            catch (IndexOutOfBoundsException ioobe){
+                System.out.println(ioobe.getMessage());
+                System.out.println("Túl lett indexelve");
+                ioobe.printStackTrace();
+            }
+        }
+        System.out.println("Vége");
+    }
+
+    public static void main(String[] args) {
+        FirstLetter firstLetter = new FirstLetter();
+        firstLetter.writeAllTheWords();
+    }
+}
