67bd92ae9d2a8d8cf060ca3a7b61cd11ac86253d
Keltaisten <keltaisten@gmail.com>
2021-11-18 19:54:14 +0100

    Positive updated

--- training-solutions/src/main/java/introexceptionthrow/PositiveNumber.java
+++ training-solutions/src/main/java/introexceptionthrow/PositiveNumber.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 public class PositiveNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String numberString = "0";
+        String numberString;
         System.out.println("Please write a positive whole number");
 
         numberString = scanner.nextLine();
+
+        if(numberString.length() == 0){
+            throw new IllegalArgumentException(numberString + " is Not a positive number");
+        }
 
         char[] charArray = numberString.toCharArray();
         for(char c: charArray){
@@ -16,6 +20,7 @@ public class PositiveNumber {
                 throw new IllegalArgumentException(numberString + " is Not a positive number");
             }
         }
+
         if(Integer.parseInt(numberString)==0){
             throw new IllegalArgumentException(numberString + " is Not a positive number");
         }
