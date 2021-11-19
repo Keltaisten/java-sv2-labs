cbcd4249995f5e284dddd4eef8accf5c9841ea2c
Keltaisten <keltaisten@gmail.com>
2021-11-19 15:12:22 +0100

    Holiday and Shopping

---
+++ training-solutions/src/main/java/introexceptionreadfile/Shopping.java
@@ -0,0 +1,17 @@
+package introexceptionreadfile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Shopping {
+    public static void main(String[] args) {
+        try {
+            List<String> shopping = Files.readAllLines(Paths.get("shoppinglist.txt"));
+            System.out.println(shopping);
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+}
