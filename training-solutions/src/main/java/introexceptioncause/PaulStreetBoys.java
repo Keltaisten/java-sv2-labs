9331cfd97051ea9ce892a016613bc30a8850e9a9
Keltaisten <keltaisten@gmail.com>
2021-11-20 13:40:34 +0100

    Paul

--- training-solutions/src/main/java/introexceptioncause/PaulStreetBoys.java
+++ training-solutions/src/main/java/introexceptioncause/PaulStreetBoys.java
@@ -7,44 +7,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PaulStreetBoys {
-//    public static void main(String[] args) {
-////        List<String> names = new ArrayList<>();
-//        List<String> names = new PaulStreetBoys().readTheNames();
-////        PaulStreetBoys paulStreetBoys = new PaulStreetBoys();
-////        names = paulStreetBoys.readTheNames();
-////        paulStreetBoys.toLowerNemecsek(names);
-//        new PaulStreetBoys().toLowerNemecsek(names);
-//        System.out.println(names);
-//    }
-//
-//    private void toLowerNemecsek(List<String> names) {
-//        for (String s: names){
-//            if(s.equals("Nemecsek Ernő")){
-//                s.toLowerCase();
-//            }
-//        }
-//    }
-//
-//    public List<String> readTheNames(){
-//        try {
-//            return  Files.readAllLines(Paths.get("src/main/resources/palutcaifiuk.txt"));
-//        }catch (IOException ioe){
-//            throw new IllegalStateException("File not found", ioe);
-//        }
-//    }
 
     public static void main(String[] args) {
         List<String> nameList = new PaulStreetBoys().readTheNames();
-        new PaulStreetBoys().toLowerNemecsek(nameList);
-        System.out.println(nameList);
+        System.out.println(new PaulStreetBoys().toLowerNemecsek(nameList));
     }
 
-    private void toLowerNemecsek(List<String> nameList) {
+    private List<String> toLowerNemecsek(List<String> nameList) {
         for(String s: nameList){
             if(s.equals("Nemecsek Ernő")){
                 s = s.toLowerCase();
             }
         }
+        nameList.set(nameList.indexOf("Nemecsek Ernő"),nameList.get(nameList.indexOf("Nemecsek Ernő")).toLowerCase());
+        return nameList;
     }
 
     private List<String> readTheNames() {
