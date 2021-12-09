e6a732dc891ca9b2fdda8f1c5c15363b29ccdae9
Keltaisten <keltaisten@gmail.com>
2021-12-09 13:00:10 +0100

    Math

---
+++ training-solutions/src/main/java/abstractclass/exercise/MathLesson.java
@@ -0,0 +1,16 @@
+package abstractclass.exercise;
+
+public class MathLesson {
+    public static void main(String[] args) {
+        MathExercise adder = new Adder();
+        MathExercise subtracter = new Subtracter();
+        MathExercise multiplier = new Multiplier();
+        MathExercise divisor = new Divisor();
+
+        System.out.println(adder.getSolution(2,1));
+        System.out.println(subtracter.getSolution(2,1));
+        System.out.println(multiplier.getSolution(2,1));
+        System.out.println(divisor.getSolution(2,1));
+
+    }
+}
