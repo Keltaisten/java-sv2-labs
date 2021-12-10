2c377d5057bc00c470f2fa5395172a2cd6021afc
Keltaisten <keltaisten@gmail.com>
2021-12-10 20:17:11 +0100

    Dont know if it contains junit4 exception test or not.

--- training-solutions/src/test/java/introexceptionreadfiletestjunit4/WordsTest.java
+++ training-solutions/src/test/java/introexceptionreadfiletestjunit4/WordsTest.java
@@ -2,11 +2,13 @@ package introexceptionreadfiletestjunit4;
 
 import org.junit.Test;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class WordsTest {
 
@@ -19,13 +21,15 @@ public class WordsTest {
     @Test
     public void testCreate2(){
         String wordList = new Words().getFirstWordWithA(Paths.get("src/test/resources/words.txt"));
-        assertEquals("NemAnna",wordList);
+        assertNotEquals("NemAnna",wordList);
     }
 
     @Test
     public void testCreate3(){
-        String wordList = new Words().getFirstWordWithA(Paths.get("src/test/resources/words__.txt"));
-        assertEquals("NemAnna",wordList);
+//        String wordList = new Words().getFirstWordWithA(Paths.get("src/test/resources/words__.txt"));
+//        assertEquals("NemAnna",wordList);
+        assertThrows(IllegalStateException.class,
+                () -> new Words().getFirstWordWithA(Paths.get("src/test/resources/words__.txt")));
     }
 
     @Test
@@ -37,7 +41,17 @@ public class WordsTest {
     @Test
     public void testCreate5(){
         String wordList = new Words().getFirstWordWithA(Paths.get("src/test/resources/words2.txt"));
-        assertEquals("NemAnna",wordList);
+        assertNotEquals("NemAnna",wordList);
+    }
+
+    @Test
+    public void testNotExistFile(){
+        Path path = Paths.get("src/test/resources/totallyNotWords.txt");
+
+//        IllegalStateException exception;
+
+        assertThrows(IllegalStateException.class,
+                () -> new Words().getFirstWordWithA(Paths.get("src/test/resources/words__.txt")));
     }
 
 }
