7c12fe499f58246b93851e2ae3a7295d534fcb9b
Keltaisten <keltaisten@gmail.com>
2021-12-17 00:21:37 +0100

    Discount

---
+++ training-solutions/src/main/java/enumabstract/Discount.java
@@ -0,0 +1,29 @@
+package enumabstract;
+
+public enum Discount {
+    SPECIAL_OFFER{
+        @Override
+        public double getAmountToPay(int price, int pieces){
+            return price * 0.8 * pieces;
+        }
+    }, BLACK_FRIDAY_OFFER{
+        @Override
+        public double getAmountToPay(int price, int pieces){
+            if(pieces == 1 || pieces == 2){
+                return price * 0.75 * pieces;
+            }
+            else if(pieces >= 3){
+                return price * 0.5 + (pieces - 1) * price * 0.75;
+            }
+            else{
+                throw new IllegalArgumentException("Not ok piece");
+            }
+        }
+    };
+
+    Discount(){
+
+    }
+
+    public abstract double getAmountToPay(int price, int pieces);
+}
