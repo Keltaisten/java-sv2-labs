70a53ec98793519b819b16478e426b9a8ebf984d
Keltaisten <keltaisten@gmail.com>
2021-12-16 23:28:46 +0100

    Vehicle

---
+++ training-solutions/src/main/java/interfacestaticmethods/vehicle/Vehicle.java
@@ -0,0 +1,15 @@
+package interfacestaticmethods.vehicle;
+
+public interface Vehicle {
+    public static Vehicle of(int numberOfWheels){
+        if(numberOfWheels == 2){
+            return new Bicycle(2);
+        }
+        else if(numberOfWheels == 4){
+            return new Car(4);
+        }
+        else{
+            throw new IllegalArgumentException("This is neither a bicycle nor a car.");
+        }
+    }
+}
