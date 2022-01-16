96d422b74d73ad74ecb9501af26b50251f491bd2
Keltaisten <keltaisten@gmail.com>
2022-01-16 23:09:59 +0100

    ExamResult update 2

--- training-solutions/src/main/java/collectionsclass/Exam.java
+++ training-solutions/src/main/java/collectionsclass/Exam.java
@@ -3,22 +3,27 @@ package collectionsclass;
 import java.util.*;
 
 public class Exam {
-    private List<ExamResult> examResults = new ArrayList<>();
+    private List<ExamResult> examResults;
 
     public Exam(List<ExamResult> examResults) {
         this.examResults = examResults;
     }
 
     public List<String> getNamesOfSucceededPeople(int places) {
-        List<ExamResult> temp = examResults;
+        List<ExamResult> temp = new ArrayList<>(examResults);
         Collections.sort(temp);
         Collections.reverse(temp);
-        List<String> result = new ArrayList<>();
-        for (ExamResult actual : examResults) {
-            result.add(actual.getName());
-        }
+        List<String> result = putNamesToNewList(temp);
         if (places <= examResults.size()) {
             result = result.subList(0,places);
+        }
+        return result;
+    }
+
+    private List<String> putNamesToNewList(List<ExamResult> temp) {
+        List<String> result = new ArrayList<>();
+        for (ExamResult actual : temp) {
+            result.add(actual.getName());
         }
         return result;
     }
@@ -26,4 +31,6 @@ public class Exam {
     public List<ExamResult> getExamResults() {
         return examResults;
     }
+
+
 }
